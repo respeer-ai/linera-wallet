e3bee7e53ae6100462a67bd4421214009bdd5ed6
Zhao KK <kikakkz@hotmail.com>
2024-05-22 12:15:09 +0800

    Add account info page

--- src/components/AccountInfo.vue
+++ src/components/AccountInfo.vue
@@ -1,7 +1,18 @@
 <template>
-  <div :style='{padding: "24px"}'>
+  <div>
     <div :style='{fontSize: "28px", fontWeight: "600"}' class='text-brown-8 text-center'>
       {{ accountBalance.toFixed(4) }} TLINERA
+    </div>
+    <div class='row'>
+      <q-space />
+      <q-btn
+        flat
+        label='Transer'
+        :style='{width: "240px", borderRadius: "16px", margin: "8px 0 0 0"}'
+        class='text-brown-10 bg-red-2'
+        @click='onTransferClick'
+      />
+      <q-space />
     </div>
   </div>
 </template>
@@ -13,4 +24,8 @@ import { wallet } from 'src/localstores'
 const _wallet = wallet.useWalletStore()
 
 const accountBalance = computed(() => _wallet.accountBalance(undefined, undefined))
+
+const onTransferClick = () => {
+  // TODO
+}
 </script>
