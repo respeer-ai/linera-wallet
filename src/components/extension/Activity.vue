ea4fdd90466b5bf5accc888a35cb96c86bfdcf2c
Zhao KK <kikakkz@hotmail.com>
2024-05-24 09:29:38 +0800

    Add activity page

--- src/components/extension/Activity.vue
+++ src/components/extension/Activity.vue
@@ -1,118 +1,43 @@
 <!-- eslint-disable vue/multi-word-component-names -->
 <template>
-  <div>
+  <div :style='{ width: "378px" }'>
     <div
       v-for='(activity, i) in activities'
       :key='i'
-      class='row'
+      class='cursor-pointer hover list-expand'
       :style='{
-        margin: "8px 0",
-        padding: "24px",
-        background: "rgba(255, 0, 0, 0.05)",
+        paddingTop: "8px",
+        paddingBottom: "8px",
         borderRadius: "8px",
-        fontSize: "16px",
-        lineHeight: "36px"
+        fontSize: "16px"
       }'
     >
-      <div
-        :style='{
-          width: "280px",
-          color: "#555555"
-        }'
-      >
-        <div v-if='activity.sourceAddress' class='row'>
-          <span>{{ shortid.shortId(activity.sourceAddress, addressDisplayLength) }}</span>
-          <q-icon
-            name='content_copy'
-            size='16px'
-            color='grey'
-            :style='{
-              margin: "10px 8px"
-            }'
-            class='cursor-pointer'
-            @click='onCopyClick(activity.sourceAddress)'
-          />
-        </div>
-        <strong>{{ activity.sourceAddress ? 'On ' : '' }}Microchain</strong>
-        <div class='row'>
-          <span>{{ shortid.shortId(activity.sourceChain, addressDisplayLength) }}</span>
-          <q-icon
-            name='content_copy'
-            size='16px'
-            color='grey'
-            :style='{
-              margin: "10px 8px"
-            }'
-            class='cursor-pointer'
-            @click='onCopyClick(activity.sourceChain)'
-          />
-        </div>
-      </div>
-      <div
-        :style='{
-          width: "240px",
-          color: "#555555"
-        }'
-      >
-        <div>{{ Number(activity.amount).toFixed(4) }} <strong>TLINERA</strong></div>
-        <div>{{ new Date(activity.timestamp / 1000).toLocaleDateString() }} {{ new Date(activity.timestamp / 1000).toLocaleTimeString() }}</div>
-        <div>Block <strong>{{ activity.blockHeight }}</strong></div>
-      </div>
-      <div
-        :style='{
-          width: "280px",
-          color: "#555555"
-        }'
-      >
-        <div v-if='activity.targetAddress' class='row'>
-          <span>{{ shortid.shortId(activity.targetAddress, addressDisplayLength) }}</span>
-          <q-icon
-            name='content_copy'
-            size='16px'
-            color='grey'
-            :style='{
-              margin: "10px 8px"
-            }'
-            class='cursor-pointer'
-            @click='onCopyClick(activity.targetAddress)'
-          />
-        </div>
-        <strong>{{ activity.targetAddress ? 'On ' : '' }}Microchain</strong>
-        <div class='row'>
-          <span>{{ shortid.shortId(activity.targetChain, addressDisplayLength) }}</span>
-          <q-icon
-            name='content_copy'
-            size='16px'
-            color='grey'
-            :style='{
-              margin: "10px 8px"
-            }'
-            class='cursor-pointer'
-            @click='onCopyClick(activity.targetChain)'
-          />
-        </div>
-      </div>
-      <div
-        :style='{
-          width: "280px",
-          color: "#555555"
-        }'
-      >
-        <div class='row'>
-          <span>{{ shortid.shortId(activity.certificateHash, addressDisplayLength) }}</span>
-          <q-icon
-            name='content_copy'
-            size='16px'
-            color='grey'
-            :style='{
-              margin: "10px 8px"
-            }'
-            class='cursor-pointer'
-            @click='onCopyClick(activity.certificateHash)'
-          />
-        </div>
+      <div class='row text-grey-7'>
+        <q-avatar
+          class='bg-red-1' icon='input' text-color='brown-6' size='36px'
+          :style='{marginRight: "8px"}'
+        />
         <div>
-          {{ Number(activity.grant || 0).toFixed(4) }} <strong>TLGAS</strong>
+          <div class='row'>
+            Hash <span :style='{marginLeft: "8px"}' class='text-bold'>{{ shortid.shortId(activity.certificateHash, 8) }}</span>
+            <q-icon
+              name='content_copy'
+              size='16px'
+              color='grey'
+              :style='{
+                margin: "2px 8px"
+              }'
+              class='cursor-pointer'
+              @click='onCopyClick(activity.certificateHash)'
+            />
+          </div>
+          <div :style='{fontSize: "12px"}' class='text-grey-6'>
+            {{ new Date(activity.timestamp / 1000).toLocaleDateString() }} {{ new Date(activity.timestamp / 1000).toLocaleTimeString() }}
+          </div>
+        </div>
+        <q-space />
+        <div :style='{marginLeft: "4px"}' class='text-brown-8'>
+          <strong>{{ Number(activity.amount).toFixed(4) }}</strong>
         </div>
       </div>
     </div>
@@ -120,7 +45,7 @@
 </template>
 
 <script setup lang='ts'>
-import { computed, ref } from 'vue'
+import { computed } from 'vue'
 import { wallet, notify } from 'src/localstores'
 import { shortid } from 'src/utils'
 import { copyToClipboard } from 'quasar'
@@ -129,7 +54,6 @@ const _wallet = wallet.useWalletStore()
 const activities = computed(() => _wallet.currentAccountActivities)
 
 const notification = notify.useNotificationStore()
-const addressDisplayLength = ref(10)
 
 const onCopyClick = (address: string) => {
   copyToClipboard(address)
