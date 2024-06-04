a6d25253965165c0bfeecb828ae773623e28edd3
Zhao KK <kikakkz@hotmail.com>
2024-06-04 11:50:59 +0800

    Inject linera to window

--- src-bex/dom.ts
+++ src-bex/dom.ts
@@ -1,12 +1,43 @@
 // Hooks added here have a bridge allowing communication between the Web Page and the BEX Content Script.
 // More info: https://quasar.dev/quasar-cli/developing-browser-extensions/dom-hooks
 import { bexDom } from 'quasar/wrappers'
+import log from 'loglevel'
+import { v4 as uuid } from 'uuid'
+import { initializeProvider } from '@metamask/providers'
+import { WindowPostMessageStream } from '@metamask/post-message-stream'
+import * as process from 'process'
+import * as constant from './const'
+
+window.process = process
+
+log.setDefaultLevel(process.env.NODE_ENV === 'development' ? 'debug' : 'warn')
+
+const setupProvider = () => {
+  const metamaskStream = new WindowPostMessageStream({
+    name: constant.INPAGE,
+    target: constant.CONTENT_SCRIPT
+  })
+
+  const provider = initializeProvider({
+    connectionStream: metamaskStream,
+    logger: log,
+    shouldShimWeb3: true,
+    shouldSetOnWindow: false,
+    providerInfo: {
+      uuid: uuid(),
+      name: 'CheCko Spring Hackathon',
+      icon: 'data:image/png+xml;base64',
+      rdns: 'ai.respeer.checko.main'
+    }
+  })
+
+  // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment, @typescript-eslint/no-explicit-any
+  const _window = window as any
+  // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
+  _window.linera = provider
+  window.dispatchEvent(new Event('linera#initialized'))
+}
 
 export default bexDom((/* bridge */) => {
-  /*
-  bridge.send('message.to.quasar', {
-    worked: true
-  })
-  */
-  console.log('from dom')
+  setupProvider()
 })
