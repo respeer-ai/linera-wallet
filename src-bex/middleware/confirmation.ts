166997e850ce8d6368964173f65b022e83602b7d
Zhao KK <kikakkz@hotmail.com>
2024-06-06 11:41:32 +0800

    Fix need confirm

--- src-bex/middleware/confirmation.ts
+++ src-bex/middleware/confirmation.ts
@@ -31,12 +31,12 @@ export const showPopup = async () => {
 }
 
 const confirmations = new Map<RpcMethod, boolean>([
-  [RpcMethod.GET_PROVIDER_STATE, true],
+  [RpcMethod.GET_PROVIDER_STATE, false],
   [RpcMethod.ETH_REQUEST_ACCOUNTS, true]
 ])
 
 export const needConfirm = (req: JsonRpcRequest<JsonRpcParams>): boolean => {
-  return confirmations.get(req.method as RpcMethod) || true
+  return !!confirmations.get(req.method as RpcMethod)
 }
 
 export const confirmationHandler = async (req: JsonRpcRequest<JsonRpcParams>) => {
