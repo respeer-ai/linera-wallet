9e8e4411aa21d6d09209a11cb60668c5ad048be4
Zhao KK <kikakkz@hotmail.com>
2024-06-07 20:15:26 +0800

    Reliable close window

--- src-bex/manager/notificationmanager.ts
+++ src-bex/manager/notificationmanager.ts
@@ -11,7 +11,13 @@ import {
 export default class NotificationManager {
   currentPopupId?: number
 
-  createPopupWindow (resolve: (newWindowId?: number) => void, reject: (err: Error) => void, requestId: number, params?: JsonRpcParams) {
+  createPopupWindow (
+    resolve: (newWindowId?: number) => void,
+    reject: (err: Error) => void,
+    requestId: number,
+    onWindowClosed: (requestId: number) => void,
+    params?: JsonRpcParams
+  ) {
     browser.windows.getAll()
       .then(async (windows: Windows.Window[]) => {
         const _window = windows?.find((window: Windows.Window) => {
@@ -37,10 +43,16 @@ export default class NotificationManager {
               height: NOTIFICATION_HEIGHT,
               left,
               top,
-              focused: true,
-              allowScriptsToClose: false
+              focused: true
             }).then((_window: Windows.Window) => {
               this.currentPopupId = _window.id
+              const listener = (windowId: number) => {
+                if (_window.id === windowId) {
+                  browser.windows.onRemoved.removeListener(listener)
+                  onWindowClosed(requestId)
+                }
+              }
+              browser.windows.onRemoved.addListener(listener)
               resolve(_window.id)
             }).catch((e: Error) => {
               reject(e)
@@ -54,9 +66,13 @@ export default class NotificationManager {
       })
   }
 
-  public async showPopup (requestId: number, params?: JsonRpcParams): Promise<number | undefined> {
+  public async showPopup (
+    requestId: number,
+    onWindowClosed: (requestId: number) => void,
+    params?: JsonRpcParams
+  ): Promise<number | undefined> {
     return new Promise<number | undefined>((resolve, reject) => {
-      this.createPopupWindow(resolve, reject, requestId, params)
+      this.createPopupWindow(resolve, reject, requestId, onWindowClosed, params)
     })
   }
 }
--- src-bex/middleware/confirmation.ts
+++ src-bex/middleware/confirmation.ts
@@ -2,7 +2,6 @@ import type { JsonRpcRequest, JsonRpcParams } from '@metamask/utils'
 import { RpcMethod } from './rpc'
 import NotificationManager from '../manager/notificationmanager'
 import { basebridge } from '../event'
-import { BexPayload } from '@quasar/app-vite'
 
 const notificationManager = new NotificationManager()
 
@@ -15,7 +14,7 @@ export const needConfirm = (req: JsonRpcRequest<JsonRpcParams>): boolean => {
   return !!confirmations.get(req.method as RpcMethod)
 }
 
-// DelayMs is workaround for the first message of bridge
+// TODO: DelayMs is workaround for the first message of bridge
 const confirmationWithExistPopup = (requestId: number, resolve: () => void, reject: (err: Error) => void, delayMs: number) => {
   setTimeout(() => {
     basebridge.EventBus.bridge?.send('popup.new', { requestId })
@@ -28,17 +27,6 @@ const confirmationWithExistPopup = (requestId: number, resolve: () => void, reje
   }, delayMs)
 }
 
-interface PopupNewResponse {
-  requestId: number
-}
-
-// It's not work very good
-const newPopupHandler = (reject: (err: Error) => void): (payload: BexPayload<PopupNewResponse, undefined>) => void => {
-  return (payload: BexPayload<PopupNewResponse, undefined>) => {
-    if (payload.data.requestId === undefined) reject(new Error('Rejected by user'))
-  }
-}
-
 export const confirmationHandler = async (req: JsonRpcRequest<JsonRpcParams>): Promise<void> => {
   if (!needConfirm(req)) {
     return await Promise.resolve(undefined)
@@ -47,18 +35,13 @@ export const confirmationHandler = async (req: JsonRpcRequest<JsonRpcParams>): P
   return new Promise<void>((resolve, reject) => {
     const requestId = Number(req.id)
 
-    const _newPopupHandler = newPopupHandler(reject)
-    basebridge.EventBus.bridge?.once('popup.done', _newPopupHandler)
-
-    notificationManager.showPopup(requestId, req.params)
+    notificationManager.showPopup(requestId, (_requestId: number) => {
+      if (requestId === _requestId) {
+        return reject(new Error('Rejected by user'))
+      }
+    }, req.params)
       .then((newWindowId?: number) => {
-        confirmationWithExistPopup(requestId, () => {
-          basebridge.EventBus.bridge?.off('popup.done', _newPopupHandler)
-          resolve()
-        }, (e: Error) => {
-          basebridge.EventBus.bridge?.off('popup.done', _newPopupHandler)
-          reject(e)
-        }, newWindowId !== undefined ? 1000 : 0)
+        confirmationWithExistPopup(requestId, resolve, reject, newWindowId !== undefined ? 1000 : 0)
       })
       .catch((e: Error) => {
         reject(e)
