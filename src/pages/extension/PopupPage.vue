5e009dec824328c09ccbbc4121916e6f560e813b
Zhao KK <kikakkz@hotmail.com>
2024-06-07 23:21:18 +0800

    Add request types

--- src/pages/extension/PopupPage.vue
+++ src/pages/extension/PopupPage.vue
@@ -1,28 +1,18 @@
 <template>
   <div>
     <PupupHeader />
-    {{ params }}
   </div>
 </template>
 <script setup lang='ts'>
 import { useQuasar } from 'quasar'
-import { computed, onMounted, onUnmounted } from 'vue'
+import { onMounted, onUnmounted } from 'vue'
 import { BexPayload } from '@quasar/app-vite'
-import { useRoute } from 'vue-router'
 import { popup } from 'src/localstores'
 import { commontypes } from 'src/types'
 
 import PupupHeader from 'src/components/extension/PupupHeader.vue'
 
-interface Query {
-  requestId: string | number
-  params?: string
-}
-
 const quasar = useQuasar()
-const route = useRoute()
-const params = computed(() => (route.query as unknown as Query).params)
-
 const _popup = popup.usePopupStore()
 
 const handleNewRequest = (payload: BexPayload<commontypes.PopupRequest, unknown>) => {
