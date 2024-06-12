98aa2f0174c3f3542eddd4e55f6d6e7538635a95
Zhao KK <kikakkz@hotmail.com>
2024-06-12 16:04:11 +0800

    Return current origin auth info

--- src-bex/middleware/rpcimpl/getproviderstate.ts
+++ src-bex/middleware/rpcimpl/getproviderstate.ts
@@ -1,8 +1,19 @@
-export const getProviderStateHandler = async () => {
-  return Promise.resolve({
-    isUnlocked: true,
-    accounts: [],
-    chainId: '0x10',
-    networkVersion: '12734'
-  })
+import { sharedStore } from 'app/src-bex/store'
+import { RpcRequest } from '../types'
+
+export const getProviderStateHandler = async (request: RpcRequest) => {
+  try {
+    const auth = await sharedStore.getRpcAuth(request.origin)
+    if (!auth) {
+      return Promise.reject(new Error('Invalid rpc auth'))
+    }
+    return Promise.resolve({
+      isUnlocked: true,
+      accounts: [auth?.publicKey],
+      chainId: '0x' + auth?.chainId,
+      networkVersion: '12734'
+    })
+  } catch (e) {
+    return Promise.reject(new Error(JSON.stringify(e)))
+  }
 }
