187b3f6cfb14ca5d9c5d38db6cd432dd9363263c
Zhao KK <kikakkz@hotmail.com>
2024-06-30 19:09:34 +0800

    Add execute_operation_with_messages to wallet extension

--- wasm/src/lib.rs
+++ wasm/src/lib.rs
@@ -19,21 +19,23 @@ use linera_base::{
 };
 use linera_chain::data_types::IncomingMessage;
 use linera_core::{
-    client::{ArcChainClient, ChainClient, ChainClientBuilder},
-    node::{
-        LocalValidatorNode as _,
-        LocalValidatorNodeProvider as _,
-    },
+    client::{ArcChainClient, ChainClient, Client}, data_types::RawBlockProposal, node::{
+        CrossChainMessageDelivery, LocalValidatorNode as _, LocalValidatorNodeProvider as _
+    }
 };
 use linera_execution::Operation;
 use linera_rpc::node_provider::{NodeOptions, NodeProvider};
+use linera_views::views::ViewError;
 
+
+use linera_storage::Storage;
 use wasm_bindgen::prelude::*;
 use web_sys::*;
 
 use std::collections::{BTreeMap, HashMap};
-use std::sync::RwLock;
+use std::sync::{RwLock, Arc};
 use std::time::Duration;
+use tokio::task::JoinSet;
 
 mod config;
 mod options;
@@ -58,44 +60,80 @@ pub async fn get_storage() -> Result<WebStorage, <WebStorage as linera_storage::
     ).await
 }
 
-pub struct ClientContext {
+pub struct ClientContext<S>
+where
+    S: linera_storage::Storage,
+    ViewError: From<S::ContextError>,
+{
     wallet_state: WalletState,
-    chain_client_builder: ChainClientBuilder<NodeProvider>,
+    client: Arc<Client<NodeProvider, S>>,
     send_timeout: Duration,
     recv_timeout: Duration,
     notification_retry_delay: Duration,
     notification_retries: u32,
-    prng: Box<dyn CryptoRng>,
+    chain_listeners: JoinSet<()>,
 }
 
-impl ClientContext {
+impl<S> ClientContext<S>
+where
+    S: Storage + Clone + Send + Sync + 'static,
+    ViewError: From<S::ContextError>,
+{
     fn wallet_state(&self) -> &WalletState {
         &self.wallet_state
     }
 
-    fn make_chain_client<S>(&self, storage: S, chain_id: ChainId) -> ChainClient<NodeProvider, S> {
-        let chain = self
-            .wallet_state.0
-            .get(chain_id)
-            .unwrap_or_else(|| panic!("Unknown chain: {}", chain_id));
-        let known_key_pairs = chain
-            .key_pair
-            .as_ref()
-            .map(|kp| kp.copy())
-            .into_iter()
-            .collect();
-        self.chain_client_builder.build(
-            chain_id,
-            known_key_pairs,
-            storage,
-            self.wallet_state.0.genesis_admin_chain(),
-            chain.block_hash,
-            chain.timestamp,
-            chain.next_block_height,
-            chain.pending_block.clone(),
-            chain.pending_blobs.clone(),
-        )
+    pub fn new(storage: S, options: &ClientOptions, wallet_state: WalletState) -> Self {
+        let node_options = NodeOptions {
+            send_timeout: options.send_timeout,
+            recv_timeout: options.recv_timeout,
+            notification_retry_delay: options.notification_retry_delay,
+            notification_retries: options.notification_retries,
+        };
+        let node_provider = NodeProvider::new(node_options);
+        let delivery = CrossChainMessageDelivery::new(options.wait_for_outgoing_messages);
+        let client = Arc::new(
+            Client::new(
+                node_provider,
+                storage,
+                options.max_pending_messages,
+                delivery,
+            )
+            .with_message_policy(options.message_policy),
+        );
+        ClientContext {
+            client,
+            wallet_state,
+            send_timeout: options.send_timeout,
+            recv_timeout: options.recv_timeout,
+            notification_retry_delay: options.notification_retry_delay,
+            notification_retries: options.notification_retries,
+            chain_listeners: JoinSet::new(),
+        }
     }
+
+    fn make_chain_client(&self, chain_id: ChainId) -> ChainClient<NodeProvider, S> {
+      let chain = self
+          .wallet_state.0
+          .get(chain_id)
+          .unwrap_or_else(|| panic!("Unknown chain: {}", chain_id));
+      let known_key_pairs = chain
+          .key_pair
+          .as_ref()
+          .map(|kp| kp.copy())
+          .into_iter()
+          .collect();
+      self.client.build(
+          chain_id,
+          known_key_pairs,
+          self.wallet_state.0.genesis_admin_chain(),
+          chain.block_hash,
+          chain.timestamp,
+          chain.next_block_height,
+          chain.pending_block.clone(),
+          chain.pending_blobs.clone(),
+      )
+  }
 
     fn update_wallet_for_new_chain(
         &mut self,
@@ -116,18 +154,14 @@ impl ClientContext {
         }
     }
 
-    pub async fn update_and_save_wallet<'a, S>(&mut self, state: &mut ChainClient<NodeProvider, S>)
+    pub async fn update_and_save_wallet<'a>(&mut self, state: &mut ChainClient<NodeProvider, S>)
     where
         S: linera_storage::Storage + Clone + Send + Sync + 'static,
         linera_views::views::ViewError: From<S::ContextError>,
     {
         self.wallet_state.0.update_from_state(state).await;
-        self.wallet_state.0.refresh_prng_seed(&mut self.prng);
         self.wallet_state.write();
     }
-
-
-
 
     pub fn make_node_provider(&self) -> NodeProvider {
         NodeProvider::new(self.make_node_options())
@@ -143,28 +177,12 @@ impl ClientContext {
     }
 }
 
-pub async fn get_client_context(wallet: &Wallet) -> Result<ClientContext, JsError> {
+pub async fn get_client_context(wallet: &Wallet) -> Result<ClientContext<WebStorage>, JsError> {
+    let mut storage = get_storage().await.expect("invalid storage");
     // TODO get from config
     let options: ClientOptions = options::OPTIONS;
 
-    Ok(ClientContext {
-        prng: wallet.make_prng(),
-        wallet_state: WalletState(wallet.copy()),
-        chain_client_builder: ChainClientBuilder::new(
-            NodeProvider::new(NodeOptions {
-                send_timeout: options.send_timeout,
-                recv_timeout: options.recv_timeout,
-                notification_retry_delay: options.notification_retry_delay,
-                notification_retries: options.notification_retries,
-            }),
-            options.max_pending_messages,
-            linera_core::node::CrossChainMessageDelivery::new(true),
-        ),
-        send_timeout: options.send_timeout,
-        recv_timeout: options.recv_timeout,
-        notification_retry_delay: options.notification_retry_delay,
-        notification_retries: options.notification_retries,
-    })
+    Ok(ClientContext::new(storage, &options, WalletState(wallet.copy())))
 }
 
 struct WalletState(Wallet);
@@ -190,9 +208,9 @@ pub async fn dapp_query_validators() -> Result<(), JsError> {
 
     let chain_id = wallet.default_chain().expect("No default chain");
 
-    let mut client_context: ClientContext = get_client_context(wallet).await?;
+    let mut client_context: ClientContext<WebStorage> = get_client_context(wallet).await?;
 
-    let mut chain_client = client_context.make_chain_client(storage, chain_id);
+    let mut chain_client = client_context.make_chain_client(chain_id);
     log::info!(
         "Querying the validators of the current epoch of chain {}",
         chain_id
@@ -240,8 +258,26 @@ pub async fn dapp_query(n: u32) -> u32 {
 
 // Execute operation to get
 #[wasm_bindgen]
-pub async fn execute_operation_with_messages(operation: Operation, messages: Vec<IncomingMessage>) -> u32 {
-    n + 1
+pub async fn execute_operation_with_messages(chain_id: &str, operation: &str, messages: &str) -> Result<Option<String>, JsError> {
+  let chain_id: ChainId = serde_json::from_str(chain_id)?;
+  let operation: Operation = serde_json::from_str(operation)?;
+  let messages: Vec<IncomingMessage> = serde_json::from_str(messages)?;
+
+  let wallet = WALLET.read()?;
+  let wallet = wallet.as_ref().ok_or(JsError::new("no wallet set"))?;
+  let mut storage = get_storage().await?;
+  wallet.genesis_config().initialize_storage(&mut storage).await?;
+  let mut client_context: ClientContext<WebStorage> = get_client_context(wallet).await?;
+  let mut chain_client = client_context.make_chain_client(chain_id);
+
+  chain_client.execute_block_without_block_proposal(messages, [operation].to_vec()).await?;
+  match chain_client.peek_candidate_block_proposal().await {
+    Some(block_proposal) => {
+      let json = serde_json::to_string(block_proposal)?;
+      Ok(Some(String::from(json)))
+    }
+    _ => Ok(None),
+  }
 }
 
 #[wasm_bindgen(start)]
