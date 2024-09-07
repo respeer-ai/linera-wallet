c6d4e00a2dc84e2f7429d3789b5457ead43de116
Zhao KK <kikakkz@hotmail.com>
2024-09-07 18:02:52 +0800

    Wrapper low level

--- src/localstores/index.ts
+++ src/localstores/index.ts
@@ -1,6 +1,24 @@
-export * as wallet from './wallet'
-export * as notify from './notify'
-export * as persistentsetting from './setting/persistent'
-export * as oneshotsetting from './setting/oneshot'
-export * as popup from './popup'
-export * as auth from './auth'
+import { useAuthStore } from './auth'
+import { useNotificationStore, notify, NotifyType } from './notify'
+import { usePopupStore } from './popup'
+import { useSettingStore as useOneShotSettingStore } from './setting/oneshot'
+import { useSettingStore as usePersistentSettingStore } from './setting/persistent'
+import { useWalletStore } from './wallet'
+
+const wallet = useWalletStore()
+const notification = useNotificationStore()
+const persistentSetting = usePersistentSettingStore()
+const oneShotSetting = useOneShotSettingStore()
+const popup = usePopupStore()
+const auth = useAuthStore()
+
+export const localStore = {
+  wallet,
+  notification,
+  notify,
+  NotifyType,
+  persistentSetting,
+  oneShotSetting,
+  popup,
+  auth
+}
