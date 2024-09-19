664886dfcc4e59028b817dd7954520c73d31b9d5
Zhao KK <kikakkz@hotmail.com>
2024-09-19 12:24:21 +0800

    Add default account for new account

--- src/components/account/ImportPrivateKeyInnerView.vue
+++ src/components/account/ImportPrivateKeyInnerView.vue
@@ -4,9 +4,15 @@
       Imported account won't be bound to current mnemonic.
     </div>
     <div class='vertical-sections-margin text-grey-8 text-bold'>
-      Private key
+      Account name
     </div>
     <div class='vertical-items-margin'>
+      <q-input outlined dense v-model='accountName' />
+    </div>
+    <div class='vertical-items-margin text-grey-8 text-bold'>
+      Private key
+    </div>
+    <div class='vertical-items-margin extra-margin-bottom'>
       <q-input outlined dense v-model='privateKey' :type='showPlainText ? "text" : "password"'>
         <template #append>
           <q-icon :name='showPlainText ? "bi-eye-fill" : "bi-eye-slash-fill"' class='cursor-pointer' @click='showPlainText = !showPlainText' />
@@ -14,7 +20,7 @@
       </q-input>
     </div>
     <q-btn
-      flat dense class='btn full-width vertical-sections-margin' @click='onImportClick'
+      flat dense class='btn full-width vertical-items-margin' @click='onImportClick'
       no-caps :disable='privateKey.length === 0'
     >
       Import
@@ -26,24 +32,29 @@
       Cancel
     </q-btn>
     <PasswordBridge ref='passwordBridge' v-model:password='password' />
+    <GenerateKey ref='generateKey' />
     <OwnerBridge ref='ownerBridge' />
   </div>
 </template>
 
 <script setup lang='ts'>
-import { ref } from 'vue'
+import { onMounted, ref } from 'vue'
 import { Ed25519SigningKey, Memory } from '@hazae41/berith'
 import { _hex } from 'src/utils'
 import { localStore } from 'src/localstores'
 
 import PasswordBridge from '../bridge/db/PasswordBridge.vue'
 import OwnerBridge from '../bridge/db/OwnerBridge.vue'
+import GenerateKey from './GenerateKey.vue'
 
 const privateKey = ref('')
+const accountName = ref('')
+
 const showPlainText = ref(false)
 const password = ref(undefined as unknown as string)
 
 const ownerBridge = ref<InstanceType<typeof OwnerBridge>>()
+const generateKey = ref<InstanceType<typeof GenerateKey>>()
 
 const emit = defineEmits<{(ev: 'imported'): void,
   (ev: 'canceled'): void
@@ -70,5 +81,10 @@ const onImportClick = async () => {
 const onCancelClick = () => {
   emit('canceled')
 }
+
+onMounted(async () => {
+  // eslint-disable-next-line @typescript-eslint/no-unsafe-argument, @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-call
+  accountName.value = await generateKey.value?.defaultAccountName() as string
+})
 
 </script>
