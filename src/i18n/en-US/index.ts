c535ea19bc0815886e56d2c19b984e2df4a1808e
王荷青 <clarence@wangheqingdeMacBook-Pro.local>
2024-11-11 16:11:12 +0800

    update i18n

--- src/i18n/en-US/index.ts
+++ src/i18n/en-US/index.ts
@@ -11,7 +11,7 @@ export default {
   MSG_CHECKO_CANNOT_RECOVERY_YOUR_WALLET:
     'CheCko cannot recover your wallet. We will use your secret ' +
     'recovery phrase to validate your ownership, restore your wallet ' +
-    'and setup a new password.',
+    'and set up a new password.',
   MSG_PASTE_RECOVERY_PHRASE_INTO_ANY_FIELD:
     'You can paste your secret recovery phrase into any field.',
   MSG_IMPORTED_ACCOUNT_WONT_BOUND_TO_MNEMONIC:
@@ -23,7 +23,7 @@ export default {
   MSG_ADD_LEDGER: 'Add ledger',
   MSG_WRITE_DOWN_RECOVERY_PHRASE: 'Write down your Secret<br>Recovery Phrase',
   MSG_WRITE_DOWN_24_WORDS_RECOVERY_PHRASE_AND_SAVE_IT:
-    'Write donw this 24-words Secret Recovery Phrase and save it in a place that you trust and only you can access.',
+    'Write down this 24-words Secret Recovery Phrase and save it in a place that you trust and only you can access.',
   MSG_TIPS: 'Tips',
   MSG_COLON: ':',
   MSG_SAVE_IN_A_PASSWORD_MANAGER: 'Save in a password manager',
@@ -41,16 +41,16 @@ export default {
   MSG_PRIVATE_KEY_PROVIDE_FULL_PERMISSIONS:
     'This private key provide <strong>full permissions</strong> to its account assets.',
   MSG_NEVER_SHARE_PRIVATE_KEY_TO_ANYONE_ELSE:
-    '<strong>Please never share private key to anyone else.</strong> ' +
-    "CheCko support team won't require to provide those information.",
+    '<strong>Please never share private key with anyone else.</strong> ' +
+    "CheCko support team won't be required to provide that information.",
   MSG_PHISHING_USER_USE_IT_TO_STEAL_ASSETS:
     "<strong class='text-blue-8'>But, phishing users use it to steal your assets!</strong>",
   MSG_CONFIRMING: 'Confirming',
   MSG_VALIDATE_SECRET_RECOVERY_PHRASE: 'Validate secret recovery phrase',
   MSG_FILL_RIGHT_MNEMONIC_INTO_EMPTY_FIELDS:
-    'Fill right mnemonic into empty fields to confirm you already properly backup mnemonic.',
+    'Fill the right mnemonic into empty fields to confirm you already properly backup mnemonic.',
   MSG_DONT_SAVE_MNEMONIC_PROPERLY_SAFE_TO_CREATE_NEW:
-    "If you don't save mnemonic properly, it's safe to create a new one and discard the this one at this step.",
+    "If you don't save the mnemonic properly, it's safe to create a new one and discard this one at this step.",
   MSG_NEW_TO_LINERA_TRANSFER_TOKENS:
     "New to Linera? <span class='cursor-pointer like-link'>Transfer</span> tokens.",
   MSG_LINERA_TOKEN_SYMBOL: 'LINERA',
