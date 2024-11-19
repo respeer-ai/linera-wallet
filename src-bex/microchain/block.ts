bf890ac875c17dc87246b077f75e3a76092faca9
Zhao KK <kikakkz@hotmail.com>
2024-11-19 20:20:06 +0800

    Better loop

--- src-bex/microchain/block.ts
+++ src-bex/microchain/block.ts
@@ -3,24 +3,19 @@ import axios from 'axios'
 export class BlockSigner {
   static running = false
 
-  static delay = async (ms: number) => {
-    return new Promise(resolve => setTimeout(resolve, ms))
-  }
-
-  static async execute() {
-    while (true) {
-      // swagger-ui.internal-devops.development.npool.top
-      console.log('Execute', Date.now())
-      axios.get('http://swagger-ui.internal-devops.development.npool.top/' + Date.now().toString(), {
-        proxy: false,
-        timeout: 1000
-      }).then((resp) => {
-        console.log(resp)
-      }).catch((e) => {
-        console.log('Failed request', e)
-      })
-      await BlockSigner.delay(1000)
-    }
+  static execute() {
+    // swagger-ui.internal-devops.development.npool.top
+    console.log('Execute', Date.now())
+    axios.get('http://swagger-ui.internal-devops.development.npool.top/' + Date.now().toString(), {
+      proxy: false,
+      timeout: 1000
+    }).then((resp) => {
+      console.log(resp)
+      setTimeout(() => BlockSigner.execute(), 1000)
+    }).catch((e) => {
+      console.log('Failed request', e)
+      setTimeout(() => BlockSigner.execute(), 1000)
+    })
   }
 
   public static run() {
