a7138fe13cd9321ef10c53f7776083985dcf0aad
Zhao KK <kikakkz@hotmail.com>
2024-11-28 17:46:07 +0800

    Add policy and term of use

--- src/components/footer/FooterMenu.vue
+++ src/components/footer/FooterMenu.vue
@@ -3,8 +3,12 @@
     <div v-if='!localStore.setting.extensionMode' class='row' v-html='$t("MSG_ANOTHER_BROWSER_WALLET_FOR_LINERA_BLOCKCHAIN_BY_RESPPER_MAAS")' />
     <q-space />
     <TestnetTip />
-    <a href='https://testnet.respeer.ai/policy' class='footer-menu-margin-x-left'>{{ $t('MSG_POLICY') }}</a>
-    <a href='https://testnet.respeer.ai/terms' class='footer-menu-margin-x-left'>{{ $t('MSG_TERM_OF_USE') }}</a>
+    <div class='footer-menu-margin-x-left like-link cursor-pointer' @click='onPolicyClick'>
+      {{ $t('MSG_POLICY') }}
+    </div>
+    <div class='footer-menu-margin-x-left like-link cursor-pointer' @click='onTermOfUseClick'>
+      {{ $t('MSG_TERM_OF_USE') }}
+    </div>
     <q-space v-if='localStore.setting.extensionMode' />
   </div>
 </template>
@@ -13,5 +17,13 @@
 import { localStore } from 'src/localstores'
 
 import TestnetTip from '../testnet/TestnetTip.vue'
+
+const onPolicyClick = () => {
+  window.open('https://testnet.respeer.ai/policy')
+}
+
+const onTermOfUseClick = () => {
+  window.open('https://testnet.respeer.ai/terms')
+}
 
 </script>
