573c97f693f886acc33087fdcaecfdc3d694a0a5
Zhao KK <kikakkz@hotmail.com>
2024-12-13 09:28:09 +0800

    Support reactive balance

--- src/components/bridge/db/OwnerBalanceBridge.vue
+++ src/components/bridge/db/OwnerBalanceBridge.vue
@@ -7,6 +7,8 @@ import { dbWallet } from 'src/controller'
 import { db } from 'src/model'
 import { onMounted, ref, toRef, watch } from 'vue'
 import { dbBridge } from 'src/bridge'
+import { liveQuery } from 'dexie'
+import { useObservable } from '@vueuse/rxjs'
 
 import DbOwnerBridge from './OwnerBridge.vue'
 
@@ -21,6 +23,14 @@ const tokenId = toRef(props, 'tokenId')
 const tokenBalance = defineModel<number>('tokenBalance')
 const usdBalance = defineModel<number>('usdBalance')
 
+const _tokenBalance = useObservable<number>(
+  liveQuery(async () => {
+    const owner = (await dbWallet.owners.toArray()).find((el) => el.selected)
+    if (!owner) return 0
+    return await dbBridge.OwnerBalance.getTokenBalance(owner, tokenId.value)
+  }) as never
+)
+
 onMounted(async () => {
   const owner = (await dbWallet.owners.toArray()).find((el) => el.selected)
   if (!owner) return
@@ -32,6 +42,13 @@ watch(selectedOwner, async () => {
   const owner = (await dbWallet.owners.toArray()).find((el) => el.selected)
   if (!owner) return
   tokenBalance.value = await dbBridge.OwnerBalance.getTokenBalance(owner, tokenId.value)
+  usdBalance.value = await dbBridge.OwnerBalance.getUsdBalance(owner, tokenId.value)
+})
+
+watch(_tokenBalance, async () => {
+  tokenBalance.value = _tokenBalance.value
+  const owner = (await dbWallet.owners.toArray()).find((el) => el.selected)
+  if (!owner) return
   usdBalance.value = await dbBridge.OwnerBalance.getUsdBalance(owner, tokenId.value)
 })
 
