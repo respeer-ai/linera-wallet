19f4778a9573a6f099abad2d27bf89f0fae5d9fb
Zhao KK <kikakkz@hotmail.com>
2025-12-17 18:49:01 +0800

    Update SDK commit

--- wasm/src/lib.rs
+++ wasm/src/lib.rs
@@ -8,8 +8,6 @@ callable from all Web pages to which the Web client has been
 connected_.  Outside of their type, which is checked at call time,
 arguments to these functions cannot be trusted and _must_ be verified!
 */
-use std::time::Duration;
-
 use bip39::Mnemonic;
 
 use abi::meme::{MemeMessage, MemeOperation};
@@ -24,7 +22,6 @@ use linera_chain::{
     },
     wrapper_block::{WrapperProposalContent, WrapperProposedBlock},
 };
-use linera_client::client_options::ClientContextOptions;
 use linera_execution::Operation;
 
 use serde::Serialize;
@@ -38,45 +35,6 @@ mod fake_meme;
 use fake_meme::{MutationRoot as MemeMutationRoot, QueryRoot as MemeQueryRoot};
 
 mod signed_block;
-
-pub const OPTIONS: ClientContextOptions = ClientContextOptions {
-    send_timeout: linera_base::time::Duration::from_millis(4000),
-    recv_timeout: linera_base::time::Duration::from_millis(4000),
-    max_pending_message_bundles: 10,
-    retry_delay: linera_base::time::Duration::from_millis(1000),
-    max_retries: 10,
-    wait_for_outgoing_messages: false,
-    blanket_message_policy: linera_core::client::BlanketMessagePolicy::Accept,
-    restrict_chain_ids_to: None,
-    reject_message_bundles_without_application_ids: None,
-    reject_message_bundles_with_other_application_ids: None,
-    long_lived_services: false,
-    blob_download_timeout: linera_base::time::Duration::from_millis(1000),
-    certificate_batch_download_timeout: linera_base::time::Duration::from_millis(1000),
-    certificate_download_batch_size: linera_core::client::DEFAULT_CERTIFICATE_DOWNLOAD_BATCH_SIZE,
-    sender_certificate_download_batch_size:
-        linera_core::client::DEFAULT_SENDER_CERTIFICATE_DOWNLOAD_BATCH_SIZE,
-    chain_worker_ttl: Duration::from_secs(30),
-    sender_chain_worker_ttl: Duration::from_millis(200),
-    quorum_grace_period: linera_core::DEFAULT_QUORUM_GRACE_PERIOD,
-    max_joined_tasks: 100,
-    max_accepted_latency_ms: linera_core::client::requests_scheduler::MAX_ACCEPTED_LATENCY_MS,
-    cache_ttl_ms: linera_core::client::requests_scheduler::CACHE_TTL_MS,
-    cache_max_size: linera_core::client::requests_scheduler::CACHE_MAX_SIZE,
-    max_request_ttl_ms: linera_core::client::requests_scheduler::MAX_REQUEST_TTL_MS,
-    alpha: linera_core::client::requests_scheduler::ALPHA_SMOOTHING_FACTOR,
-    alternative_peers_retry_delay_ms: linera_core::client::requests_scheduler::STAGGERED_DELAY_MS,
-
-    // TODO(linera-protocol#2944): separate these out from the
-    // `ClientOptions` struct, since they apply only to the CLI/native
-    // client
-    wallet_state_path: None,
-    keystore_path: None,
-    with_wallet: None,
-    chrome_trace_exporter: false,
-    chrome_trace_file: None,
-    otlp_exporter_endpoint: None,
-};
 
 #[wasm_bindgen]
 pub async fn block_payload(content: &str) -> Result<String, JsError> {
